502646a0cdce4153fa34cb5fb8bac6c85d637c1b
Dominik Sito <dominik.sito@gmail.com>
2022-10-04 19:06:09 +0200

    Remove leftovers

--- test.rb
+++
@@ -1,6 +0,0 @@
-require_relative "lib/ttyhue"
-
-puts TTYHue.c "This { cosik } is {gui218}red color{/gui218} and this is {blue}blue color{/blue}"
-
-#TTYHue.preview_termcolors
-#TTYHue.preview_guicolors
